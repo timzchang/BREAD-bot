3d5ca5c2e6b928e1c532df397a5160a8a3a74b57
Tim Chang <zhaotingchang@gmail.com>
2024-09-21 14:55:10 -0700

    feat: webhook and deployment to fly

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express from "express";
+import { WebhookClient } from "discord.js";
+import cron from "node-cron";
+// import { DiscordRequest } from "../utils";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const url = process.env.WEBHOOK_URL;
+if (!url) {
+  console.error("Webhook URL is required");
+  process.exit(1);
+}
+// Initialize the WebhookClient with the webhook URL
+const webhookClient = new WebhookClient({ url });
+
+// Schedule a task to run every day at 9:00 AM
+cron.schedule("*/5 * * * * *", () => {
+  webhookClient
+    .send("Good morning! Here is your daily message.")
+    .then(() => console.log("Message sent successfully"))
+    .catch(console.error);
+});
+
+app.get("/", (req, res) => {
+  res.send("Express server with cron job is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
