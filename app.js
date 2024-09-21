f6674e6938fd5142458b5b8988ae90971fe4a485
Tim Chang <zhaotingchang@gmail.com>
2024-09-20 20:53:05 -0700

    chore: bread bot init

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 import {
   InteractionType,
   InteractionResponseType,
   verifyKeyMiddleware,
-} from 'discord-interactions';
-import { getRandomEmoji } from './utils.js';
+} from "discord-interactions";
+import {getRandomEmoji} from "./utils.js";
 
 // Create an express app
 const app = express();
@@ -16,44 +16,48 @@ const PORT = process.env.PORT || 3000;
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
-  // Interaction type and data
-  const { type, data } = req.body;
+app.post(
+  "/interactions",
+  verifyKeyMiddleware(process.env.PUBLIC_KEY),
+  async function (req, res) {
+    // Interaction type and data
+    const {type, data} = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
-
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
-
-    // "test" command
-    if (name === 'test') {
-      // Send a message into the channel where command was triggered from
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          // Fetches a random emoji to send from a helper function
-          content: `hello world ${getRandomEmoji()}`,
-        },
-      });
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+      return res.send({type: InteractionResponseType.PONG});
     }
 
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
-  }
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const {name} = data;
 
-  console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
-});
+      // "test" command
+      if (name === "test") {
+        // Send a message into the channel where command was triggered from
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            // Fetches a random emoji to send from a helper function
+            content: `hello world ${getRandomEmoji()}`,
+          },
+        });
+      }
+
+      console.error(`unknown command: ${name}`);
+      return res.status(400).json({error: "unknown command"});
+    }
+
+    console.error("unknown interaction type", type);
+    return res.status(400).json({error: "unknown interaction type"});
+  }
+);
 
 app.listen(PORT, () => {
-  console.log('Listening on port', PORT);
+  console.log("Listening on port", PORT);
 });
--- examples/command.js
+++ examples/command.js
@@ -1,28 +1,36 @@
-import 'dotenv/config';
-import express from 'express';
-import { InteractionType, InteractionResponseType, verifyKeyMiddleware } from 'discord-interactions';
-import { DiscordRequest } from '../utils.js';
+import "dotenv/config";
+import express from "express";
+import {
+  InteractionType,
+  InteractionResponseType,
+  verifyKeyMiddleware,
+} from "discord-interactions";
+import {DiscordRequest} from "../utils.js";
 
 // Create and configure express app
 const app = express();
 
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), function (req, res) {
-  // Interaction type and data
-  const { type, data } = req.body;
-  /**
-   * Handle slash command requests
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    // Slash command with name of "test"
-    if (data.name === 'test') {
-      // Send a message as response
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: { content: 'A wild message appeared' },
-      });
+app.post(
+  "/interactions",
+  verifyKeyMiddleware(process.env.PUBLIC_KEY),
+  function (req, res) {
+    // Interaction type and data
+    const {type, data} = req.body;
+    /**
+     * Handle slash command requests
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      // Slash command with name of "test"
+      if (data.name === "test") {
+        // Send a message as response
+        return res.send({
+          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {content: "A wild message appeared"},
+        });
+      }
     }
   }
-});
+);
 
 async function createCommand() {
   const appId = process.env.APP_ID;
@@ -39,8 +47,8 @@ async function createCommand() {
    */
   // const guildEndpoint = `applications/${appId}/guilds/<your guild id>/commands`;
   const commandBody = {
-    name: 'test',
-    description: 'Just your average command',
+    name: "test",
+    description: "Just your average command",
     // chat command (see https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types)
     type: 1,
   };
@@ -48,17 +56,17 @@ async function createCommand() {
   try {
     // Send HTTP request with bot token
     const res = await DiscordRequest(globalEndpoint, {
-      method: 'POST',
+      method: "POST",
       body: commandBody,
     });
     console.log(await res.json());
   } catch (err) {
-    console.error('Error installing commands: ', err);
+    console.error("Error installing commands: ", err);
   }
 }
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 
   createCommand();
 });
