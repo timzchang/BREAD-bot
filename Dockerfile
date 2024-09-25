7b8e66c341c696395d9e2ff109382471e00396c2
Tim Chang <zhaotingchang@gmail.com>
2024-09-24 22:16:25 -0700

    feat: add reading of csv; smaller deploys

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 # Adjust NODE_VERSION as desired
 ARG NODE_VERSION=22.9.0
-FROM node:${NODE_VERSION}-slim as base
+FROM node:${NODE_VERSION}-slim AS base
 
 LABEL fly_launch_runtime="Node.js"
 
@@ -14,7 +14,7 @@ ENV NODE_ENV="production"
 
 
 # Throw-away build stage to reduce size of final image
-FROM base as build
+FROM base AS build
 
 # Install packages needed to build node modules
 RUN apt-get update -qq && \
@@ -38,7 +38,29 @@ RUN npm prune --omit=dev
 FROM base
 
 # Copy built application
-COPY --from=build /app /app
+#13 [build 5/7] RUN ls
+#13 0.159 Dockerfile
+#13 0.159 LICENSE
+#13 0.159 README.md
+#13 0.159 assets
+#13 0.159 build
+#13 0.159 commands.js
+#13 0.159 examples
+#13 0.159 fly.toml
+#13 0.159 game.js
+#13 0.159 lib
+#13 0.159 node_modules
+#13 0.159 old-app.js
+#13 0.159 package-lock.json
+#13 0.159 package.json
+#13 0.159 renovate.json
+#13 0.159 src
+#13 0.159 tsconfig.json
+#13 0.159 utils.js
+COPY --from=build /app/build /app/build
+COPY --from=build /app/package.json /app/package.json
+COPY --from=build /app/node_modules /app/node_modules
+COPY --from=build /app/assets /app/assets
 
 # Start the server by default, this can be overwritten at runtime
 EXPOSE 3000
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,33 @@ import "dotenv/config";
 import express from "express";
 import {WebhookClient} from "discord.js";
 import cron from "node-cron";
-// import { DiscordRequest } from "../utils";
+import fs from "fs";
+import path from "path";
+import csv from "csv-parser";
+import {fileURLToPath} from "url";
+
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export function getTodaysVerse(
+  callback: (verse: string, link: string) => void
+) {
+  const today = new Date().toISOString().split("T")[0]; // Get today's date in YYYY-MM-DD format
+  const csvFilePath = path.resolve(__dirname, "../assets/verses.csv");
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on("data", (row) => {
+      if (row.date === today) {
+        console.log(row);
+        callback(row.verse, row.link);
+      }
+    })
+    .on("end", () => {
+      console.log("CSV file successfully processed");
+    });
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,20 +42,15 @@ if (!url) {
 const webhookClient = new WebhookClient({url});
 
 // Schedule a task to run every day at 9:00 AM
-cron.schedule("0 9 * * *", () => {
-  // open verses.csv
-  // date, verse, link_to_bible_gateway
-  // pick current date's verse
-  // send to channel
-  /* 
-    const {verse, link} = verses[Date.today]
-
-    webhookClient.send(`Today's Bread Reading is ${verse}: ${link}`)
-  */
-  webhookClient
-    .send("Good morning! Here is your daily message.")
-    .then(() => console.log("Message sent successfully"))
-    .catch(console.error);
+// cron.schedule("0 9 * * *", () => {
+// for testing
+cron.schedule("*/5 * * * * *", () => {
+  getTodaysVerse((verse, link) => {
+    webhookClient
+      .send(`Today's Bread Reading is ${verse}: ${link}`)
+      .then(() => console.log("Message sent successfully"))
+      .catch(console.log);
+  });
 });
 
 app.get("/", (req, res) => {
